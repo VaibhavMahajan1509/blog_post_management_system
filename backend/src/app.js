d903eac8ca11cb7da3baa94e3ca1fe18a7dd3c91
Vaibhav <vaibhav.mahajan1509@email.comgit config -listgit config --global user.email vaibhav.mahajan1509@email.com>
2026-05-22 18:33:20 +0530

    case sensitivity issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,20 +6,8 @@ import postRoutes from "./routes/postRoutes.js";
 const app = express();
 
 
-// allowed frontend origins
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://your-frontend-domain.vercel.app",
-];
-
-
 // middlewares
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
@@ -28,7 +16,7 @@ app.use(express.json());
 app.use("/api/posts", postRoutes);
 
 
-// defult route
+// default route
 app.get("/", (req, res) => {
   res.send("Blog API is running...");
 });
