9c96a546d6bc12e714ea3cb4e3d89f2964f511d8
Vaibhav <vaibhav.mahajan1509@email.comgit config -listgit config --global user.email vaibhav.mahajan1509@email.com>
2026-05-22 18:46:02 +0530

    secure cors setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,24 @@
 import express from "express";
 import cors from "cors";
-
 import postRoutes from "./routes/postRoutes.js";
 
 const app = express();
 
 
+// allowed origins 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://blog-post-management-system.vercel.app/", 
+];
+
+
 // middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
